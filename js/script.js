f32bdc5f03121cedde50ebbc89dcddb413e938d2
isabelle may salmon <845868806@bun2-206-13.local>
2018-03-27 09:35:40 +1300

    first final git push before live

--- js/script.js
+++ js/script.js
@@ -2,9 +2,6 @@ $(document).ready(function(){
     console.log("Js loaded");
     $("#hamburgerHeader").click(function(){
         console.log("Hamburger is clicked");
-        $(".side-nav").addClass('side-nav-width');
-    });
-    $("#hamburgerNav").click(function(){
-        $(".side-nav").removeClass('side-nav-width');
+        $(".side-nav").toggleClass('side-nav-width');
     });
 });
